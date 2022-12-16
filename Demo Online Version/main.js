ad8238466f4f66396bbb01abb7114cd0932cdd77
T1fff <98428313+T1fff@users.noreply.github.com>
2022-12-16 07:34:57 -0500

    Add files via upload

---
+++ Demo Online Version/main.js
@@ -0,0 +1,34 @@
+console.log("TASK 1: CHESSBOARD")
+const drawChessboard = require("../Demo Online Version/src/task1").drawChessboard
+console.log(drawChessboard)
+console.log()
+
+console.log("TASK 2: ENCLOSING ENVELOPES")
+const enclosingEnvelopes = require("../Demo Online Version/src/task2.js").enclosingEnvelopes
+console.log(enclosingEnvelopes)
+console.log()
+
+console.log("TASK 3: SORTING TRIANGLES")
+const sortingTriangles = require("../Demo Online Version/src/task3.js").sortingTriangles
+console.log(sortingTriangles)
+console.log()
+
+console.log("TASK 4: PALINDROME")
+const findPalindrome = require("../Demo Online Version/src/task4").findPalindrome
+console.log(findPalindrome)
+console.log()
+
+console.log("TASK 5: LUCKY TICKETS")
+const findingTicketType = require("../Demo Online Version/src/task5").findingTicketType
+console.log(findingTicketType)
+console.log()
+
+console.log("TASK 6: SERIES OF SQUARED NUMBERS")
+const seriesOfSquaredNumbers = require("../Demo Online Version/src/task6").seriesOfSquaredNumbers
+console.log(seriesOfSquaredNumbers)
+console.log()
+
+console.log("TASK 7: FIBONACCI")
+const fibonacciSeries = require("../Demo Online Version/src/task7").fibonacciSeries
+console.log(fibonacciSeries)
+console.log()
---
+++ Demo Online Version/style.js
@@ -0,0 +1,3 @@
+gsap.from(".title", {opacity: 0, duration: 2, delay: 1.5, y:25})
+gsap.from(".author", {opacity: 0, duration: 2, delay: 1.5, y:25})
+gsap.from(".buttondiv", {opacity: 0, duration: 2, delay: 1.5, y:40})
